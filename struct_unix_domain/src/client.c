6819e8bc594a879af29abb621ff16796233671a7
Kahlveen <kahlveen321@gmail.com>
2016-12-30 19:13:36 +0800

    Struct over unix domain step1

---
+++ struct_unix_domain/src/client.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string.h>
+#include "cli.h"
+
+void usage(char* prog_name){
+	printf("Usage:%s [info | stats]\n\n", prog_name);
+}
+
+int main(int argc, char **argv)
+{
+	enum command cmd = UNDEFINED;
+
+	if(argc != 2){
+		usage(argv[0]);
+		exit(1);
+	}
+
+	if(strcmp(argv[1], "info") == 0)
+		cmd = INFO;
+
+	else if(strcmp(argv[1], "stats") == 0)
+		cmd = STATS;
+
+	else {
+		usage(argv[0]);
+		exit(1);
+	}
+
+
+	struct sockaddr_un addr;
+	char buf[100];
+	char *cli_socket = "/tmp/cli_test";
+	int fd, cl;
+
+	if((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1){
+		perror("socket error");
+		exit(-1);
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path,cli_socket,sizeof(addr.sun_path)-1);
+
+	if(connect(fd,(struct sockaddr*)&addr, sizeof(addr))){
+		perror("connect error");
+		exit(-1);
+	}
+
+	int num_read = 0;
+
+	if(write(fd,&cmd, sizeof(cmd)) != sizeof(cmd)){
+		printf("Cannot write command over");
+		exit(-1);
+	}
+
+	int size_buf = 0;
+	if( (num_read = read(fd,&size_buf,sizeof(size_buf))) > 0)
+		printf("Size_buf: %d\n",size_buf);
+
+	return 0;
+}
+
---
+++ struct_unix_domain/src/server.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string.h>
+#include "cli.h"
+
+int main()
+{
+	struct sockaddr_un addr;
+	char buf[100];
+	char *cli_socket = "/tmp/cli_test";
+	int fd, cl;
+
+	if((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1){
+		perror("socket error");
+		exit(-1);
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path,cli_socket,sizeof(addr.sun_path)-1);
+	unlink(cli_socket);
+
+	if(bind(fd,(struct sockaddr*)&addr, sizeof(addr))){
+		perror("bind error");
+		exit(-1);
+	}
+
+	if(listen(fd,1)){
+		perror("listen error");
+		exit(-1);
+	}
+
+	while(1){
+		if( (cl = accept(fd, NULL, NULL)) == -1){
+			perror("accept error");
+			continue;
+		}
+
+		int num_read = 0;
+		int size_buf = 0;
+		enum command cmd;
+
+		if( ( num_read = read(cl, &cmd, sizeof(cmd))) > 0) {
+			if(cmd == INFO){
+				printf("INFO cmd\n");
+				size_buf = 10;
+				if(write(cl,&size_buf, sizeof(size_buf)) != sizeof(size_buf)){
+					printf("Write error");
+				}
+			}
+
+			else if(cmd == STATS){
+				printf("STATS cmd\n");
+				size_buf = 999;
+				if(write(cl,&size_buf, sizeof(size_buf)) != sizeof(size_buf)){
+					printf("Write error");
+				}
+			}
+		}
+
+		else if(num_read == -1){
+			perror("Read");
+			exit(-1);
+		}
+
+		else if(num_read == 0){
+			printf("EOF\n");
+			close(cl);
+		}
+		sleep(1);
+	}
+	return 0;
+}
